9407597bd0642d53c6e8cce116e01743b3fc6aec
Ghosin <ghosin2001@outlook.com>
2018-11-29 18:55:34 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#-----------------------------------
+#       jsdelivr_clone配置文件
+#  client_id和client_secret由github 
+#  OAuth Application得到 需创建后填入
+#-----------------------------------
+client_id = ''
+client_secret = ''
